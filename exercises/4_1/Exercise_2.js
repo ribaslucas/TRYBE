0665cb6ceb8b765b90809e1f538747f3f9b5d960
Lucas Ribas <lucas@mundoribas.com.br>
2020-03-27 18:25:27 -0300

    Exercises (1 to 5) solutions

---
+++ exercises/4_1/Exercise_2.js
@@ -0,0 +1,10 @@
+let a = 10;
+let b = 20;
+let result = null;
+
+if (a > b) {
+    console.log("O primeiro número é maior ->  " + a)
+}
+else {
+    console.log("O segundo número é maior ->  " + b)
+}
---
+++ exercises/4_1/Exercise_5.js
@@ -0,0 +1,16 @@
+let a = 90;
+let b = 90;
+let c = 60;
+let somaAngulos = a + b + c;
+let triangulo = false;
+
+if ( a <= 0 || b <= 0 || c <= 0 ) {
+    console.log ("Não é possível formar um triângulo com esses ângulos -> " +a + " " +b + " " +c)
+}
+else if (somaAngulos === 180) {
+    triangulo = true;
+    console.log ("É possível formar um triângulo com os ângulos ->  " +a + " " +b + " " +c)
+}
+else {
+    console.log (triangulo)
+}
