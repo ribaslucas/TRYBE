3636d87b6dc740db635de19f850ef8a7998a2c4a
Lucas Ribas <lucas@mundoribas.com.br>
2020-03-28 17:31:11 -0300

    Adicionando as respostas dos exercicios 4 e 5

--- exercises/4_2/Exercise_4.js
+++ exercises/4_2/Exercise_4.js
@@ -1,8 +1,30 @@
-let n = 5;
+    // let n = 5;
 
-for (let index = 1; index <= n; index += 2) {
-    let blank = " ".repeat((n - index) / 2);
-    let l = "*".repeat(index);
-    console.log(blank + l + blank);
-}
+    // for (let index = 1; index <= n; index += 2) {
+    //     let blank = " ".repeat((n - index) / 2);
+    //     let l = "*".repeat(index);
+    //     console.log(blank + l + blank);
+    // }
 
+
+    let n = 5;
+    let lineIndex;
+    let lineColumn;
+    let lineInput = '';
+    let symbol = '*';
+    let midOfMatrix = (n + 1) / 2;
+    let controlLeft = midOfMatrix;
+    let controlRight= midOfMatrix;
+    for (lineIndex = 0; lineIndex <= midOfMatrix; lineIndex++) {
+      for (lineColumn = 1; lineColumn <= n; lineColumn++) {
+        if (lineColumn > controlRight && lineColumn < controlLeft) {
+          lineInput = lineInput + symbol;
+        } else {
+          lineInput = lineInput + ' ';
+        }
+      }
+      console.log(lineInput);
+      lineInput = '';
+      controlRight--;
+      controlLeft++;
+    
