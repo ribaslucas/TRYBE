0ca463149a454f50b20497bc17e8c8307f03ace3
Lucas Ribas <lucas@mundoribas.com.br>
2020-04-01 19:35:42 -0300

    Exercisies Part 1

--- exercises/4_3/Exercise_4.js
+++ exercises/4_3/Exercise_4.js
@@ -2,7 +2,9 @@ let info = {
     personagem: "Margarida",
     origem: "Pato Donald",
     nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+    recorrente: "Sim",
   };
 
-console.log("Bem vinda, " + info.personagem);
-
+for (let key in info) {
+    console.log(info[key])
+}
