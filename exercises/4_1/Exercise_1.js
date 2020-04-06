b7026869553e62d163a2ff9e4ac2f085f391f4de
Lucas Ribas <lucas@mundoribas.com.br>
2020-04-05 21:23:14 -0300

    Exercises 4_4 (done)

--- exercises/4_1/Exercise_1.js
+++ exercises/4_1/Exercise_1.js
@@ -3,17 +3,27 @@
 // Defining the variables//
 
 let a = 20; 
-let b = 10;
+let b = 10; 
 
 // Program 1 - Sum //
 
-let sum = a + b;
-console.log (sum);
+function soma(x, y) {
+    return x + y;
+    }
+    console.log(soma(a, b))
+
+// let sum = a + b;
+// console.log (sum);
+
 
 // Program 2 - Subtraction //
+function subtraction(e, f) {
+    return e - f 
+}
+console.log(subtraction(a, b))
 
-let subtration = a - b;
-console.log (subtration);
+// let subtration = a - b;
+// console.log (subtration);
 
 // Program 2 - Multiplication //
 
