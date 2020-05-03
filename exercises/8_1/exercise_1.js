ef273607234952ec5aae3e92dd92353987ae393e
Lucas Ribas <lucas@mundoribas.com.br>
2020-05-03 11:28:08 -0300

    Exercícios (imcompletos e sem entender) bloco 8.1

---
+++ exercises/8_1/exercise_1.js
@@ -0,0 +1,4 @@
+// Não entendia a função. Nem depois de copiar do GIt
+
+const factorial = fac => { if (fac >= 1) { return fac * factorial(fac - 1); } return 1; };
+console.log (factorial(4));
---
+++ exercises/8_1/exercise_2.js
@@ -0,0 +1,4 @@
+// Não faço ideia de como começar
+
+longestWord("Antônio foi no banheiro e não sabemos o que aconteceu") // retorna 'aconteceu'
+    
