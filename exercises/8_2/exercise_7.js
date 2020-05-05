e8442b6854d019442cd3f6a1fbc8ebe3a5cc6e14
Lucas Ribas <lucas@mundoribas.com.br>
2020-05-05 10:37:11 -0300

    Exercícios feitos e corrigidos com o gabarito

---
+++ exercises/8_2/exercise_7.js
@@ -0,0 +1,23 @@
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+// Gabarito
+const getValueByNumber = (lesson1) => Object.values(lesson1)[2];
+console.log(getValueByNumber(lesson1));
