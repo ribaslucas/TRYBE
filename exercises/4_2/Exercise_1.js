27ce0c7e5a0f3772ebf2a49f58eb4fe6b563eea8
Lucas Ribas <lucas@mundoribas.com.br>
2020-05-14 08:43:30 -0300

    Revisado até Lógica de Programação e Algoritmo

--- exercises/4_2/Exercise_1.js
+++ exercises/4_2/Exercise_1.js
@@ -1,11 +1,10 @@
-let n = 5;
-let txt = "";
+// Minha resposta após revisar o matrial
 
-for(let i = 0 ; i < n ; i++) {
-    txt += '*';
+// Nesse primeiro exercício, percorra o array imprimindo todos os valores 
+// nele contidos com a função console.log();
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let i = 0; i < numbers.length; i++ ) {
+    console.log(numbers[i]);
 }
-
-for(let i = 0 ; i < n ; i++) {
-    console.log(txt);
-}
-
--- exercises/4_2/Exercise_5.js
+++ exercises/4_2/Exercise_5.js
@@ -1,49 +1,17 @@
-// let n = 5;
-// let tBase = "*".repeat(n), spaceO = " ".repeat((n - 1) / 2); 
+// Minha resposta após revisar o matrial
 
-// if (n % 2 === 0 ) {
-//   console.log("The number must be odd!")
-// } else {
-//   console.log(spaceO + "*");
-//   for (let index = 3; index <= n - 2 ; index += 2) {
-//       spaceO = " ".repeat((n - index) / 2);
-//       let spaceI = " ".repeat(n - spaceO.length * 2 - 2);
-//       console.log(spaceO + "*" + spaceI + "*" + spaceO);
-//   }
-//   console.log(tBase);
-// }
+// Utilizando for, descubra qual o maior valor contido no array e imprima-o;
 
-let n = 7;
-let lineInput = [];
-let baseMatrix = [];
-let symbol = '*';
-let matrix = [];
-let midOfMatrix = (n + 1) / 2;
-let controlLeft = 1;
-let controlRight = n - 2;
-for (let lineColumn = 0; lineColumn < n; lineColumn++) {
-  baseMatrix[lineColumn] = symbol;
-}
-matrix[0] = baseMatrix;
-for (let lineIndex = 1; lineIndex < n; lineIndex++) {
-  for (let lineColumn = 0; lineColumn < n; lineColumn++) {
-    if (lineColumn === controlLeft || lineColumn === controlRight) {
-      lineInput[lineColumn] = symbol;
-    } else {
-      lineInput[lineColumn] = ' ';
-    }
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let maiorValor = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > maiorValor ) {
+    maiorValor = numbers[i];
   }
-  matrix[lineIndex] = lineInput;
-  controlLeft++;
-  controlRight--;
-  lineInput = [];
-  if (controlLeft > controlRight) break;
 }
-let result = '';
-for (let lineIndex = matrix.length - 1; lineIndex >= 0; lineIndex--) {
-  for (let lineColumn = 0; lineColumn < n; lineColumn++) {
-    result = result + matrix[lineIndex][lineColumn];
-  }
-  console.log(result);
-  result = '';
-}
+
+console.log(maiorValor);
+
+// Esse mesmo raciocínio tem em vários exercícios. Como no exemplo para descobrir a maior palavra.
+// A diferença é que usamos .length
