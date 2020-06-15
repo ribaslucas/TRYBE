625135c162c9187c51b8c81f8bc0b94443bacc6d
Lucas Ribas <lucas@mundoribas.com.br>
2020-06-15 09:28:19 -0300

    Bloco 13.2 - começando

---
+++ exercises/13_2/lifecycle/src/CounterDisplay.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+// import Counter from './Counter'
+
+class CounterDisplay extends React.Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    if (nextProps.value % 3 !== 0) return false;
+    return true;
+  }
+
+  render() {
+    return <div> counter: {this.props.value}</div>;
+  }
+}
+
+export default CounterDisplay;
